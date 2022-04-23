2979333e68b62dc8dbf1d630fa256ea6065f46de
Pedro Henrique <67920660+qedrohenrique@users.noreply.github.com>
2022-04-23 20:55:09 -0300

    Update 3247.py

--- BeeCrowd/3247.py
+++ BeeCrowd/3247.py
@@ -23,12 +23,13 @@ def dpPath(obstacleGrid):
     else:
       dp[i][0] = 1
       
-  for i in range(rows):
-    for j in range(cols):
+  for i in range(1,rows):
+    for j in range(1,cols):
       if(obstacleGrid[i][j] == 1):
         dp[i][j] = 0
       else:
-        dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % (2**31 - 1)
+        dp[i][j] = (dp[i-1][j] + dp[i][j-1]) 
+      
         
   return dp[rows-1][cols-1]
 
